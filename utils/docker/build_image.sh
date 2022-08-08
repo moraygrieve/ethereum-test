eda7b104403f308d0721867d1404569ded7f7526
Moray Grieve <moray.grieve@me.com>
2022-08-08 13:28:14 +0100

    Correct location

--- utils/docker/build_image.sh
+++ utils/docker/build_image.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 script_path="$(cd "$(dirname "${0}")" && pwd)"
 root_path="${script_path}/../../"
 
-docker build -t testnetobscuronet.azurecr.io/obscuronet/obscuro_test:latest -f ./image.Dockerfile "${root_path}"
+docker build -t testnetobscuronet.azurecr.io/obscuronet/obscuro_test:latest -f ${script_path}/image.Dockerfile "${root_path}"
